da32d357c26aca8ba99aab747908f2a50d9463c1
hsimonis <helmut.simonis@insight-centre.org>
2025-11-19 13:44:38 +0000

    change icon

--- demonstrator/tbischeduling/src/main/java/org/insightcentre/tbischeduling/JfxApp.java
+++ demonstrator/tbischeduling/src/main/java/org/insightcentre/tbischeduling/JfxApp.java
@@ -135,7 +135,7 @@ public class JfxApp extends GeneratedJfxApp {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.getIcons().add(new Image(JfxApp.class.getResourceAsStream("/entire.png")));
+        primaryStage.getIcons().add(new Image(JfxApp.class.getResourceAsStream("/tbischeduling.png")));
         super.start(primaryStage);
     }
 
